7a397603651f2d288c4980820f12d936fff641c6
ChaimEzra <chimas1357@gmail.com>
2025-03-11 20:49:20 +0200

    that chaim checking the connection of the server.

--- backend/Yaakov/main.py
+++ backend/Yaakov/main.py
@@ -13,5 +13,6 @@ app.include_router(session.router, prefix="/session")
 def read_root():
     return {"message": "PII Detection API Is Running"}
 
+
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
